9d658033de8ea38745111f57df1886f76aa7ca8e
Melanie Caillol <e6242482@cmaisonneuve.qc.ca>
2025-03-25 23:31:02 -0400

    Ajout d'un menu dans la page erreur et ajustement du menu externe

--- 404.php
+++ 404.php
@@ -10,10 +10,20 @@ $introuvable_img = get_theme_mod('erreur_image', 'Default Title');
            <hr class="erreur404__ligne">
            <h2>Bien tenté, mais la page que vous recherchez n'existe pas!</h2>
            <p>Veuillez vérifier s'il y a une faute d'orthographe dans votre recherche...</p>
-           <a href="front-page."></a>
-           <button class="hero__bouton erreur404__bouton">
-                Retour à l'accueil
-            </button>
+           <a href="http://localhost:81/4w4/">
+                <button class="hero__bouton erreur404__bouton">
+                    Retour à l'accueil
+                </button>
+           </a>
+           <div class="erreur404__suggestions">
+           <p>Destinations que vous aimeriez peut-être :</p>
+           <?php wp_nav_menu(array(
+                    "menu"=> "menuSuggestions",
+                    "container" => "nav",
+                    "container_class" => "erreur404__menuSuggestions"
+                )); ?>
+           </div>
+           
         </div>
     </section>
     <?php get_footer(); ?>
